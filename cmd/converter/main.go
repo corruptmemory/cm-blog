7dace3950b616e76fb9cfa6b9d447250b9eee788
Jim Powers <jim@corruptmemory.com>
2022-02-02 12:26:09 -0500

    Moved over to using the trampoline lexer

--- cmd/converter/main.go
+++ cmd/converter/main.go
@@ -9,25 +9,12 @@ import (
 
 func main() {
 	log.Println("Hello from the converter!")
-	parser, products := org.NewScanner()
 	bytes, err := os.ReadFile("content/homepage.org")
 	if err != nil {
 		log.Fatalf("bad things happened: %s", err)
 	}
-	stuff := string(bytes)
-
-	stuff1 := stuff[0 : len(stuff)/2]
-	stuff2 := stuff[len(stuff1):]
-
-	err = parser.Consume(stuff1)
-	if err != nil {
-		log.Fatalf("nope1: %s", err)
-	}
-	err = parser.Consume(stuff2)
-	if err != nil {
-		log.Fatalf("nope2: %s", err)
-	}
-	parser.EOF()
+	parser, products := org.NewScanner(string(bytes))
+	_ = parser.Scan()
 	for r := range products {
 		log.Println(r)
 	}
--- org/scanner.go
+++ org/scanner.go
@@ -12,361 +12,357 @@ var (
 	commentRegexp = regexp.MustCompile("^[ \t]*#([^+][^\n]*)")
 )
 
-type ParserError struct {
-	message string
-	line    int
-	offset  int
+type itemType int
+
+const (
+	textItem itemType = iota
+	keywordItem
+	headlineItem
+	commentItem
+)
+
+type point struct {
+	line   int
+	offset int
 }
 
-// Error to conform to the Error interface
-func (e *ParserError) Error() string {
-	return fmt.Sprintf("parse error: %s (%d:%d)", e.message, e.line, e.offset)
+func (p point) String() string {
+	return fmt.Sprintf("(%d:%d)", p.line, p.offset)
+}
+
+type span struct {
+	start point
+	end   point
+}
+
+func (s span) String() string {
+	return fmt.Sprintf("%s - %s", s.start, s.end)
+}
+
+type Item struct {
+	itemType itemType
+	span     span
+	headline
+	keyword
+	comment
+	text
+}
+
+func (i Item) String() string {
+	switch i.itemType {
+	case textItem:
+		return fmt.Sprintf("Text[%s](%s)", i.span.String(), i.text.body)
+	case headlineItem:
+		return fmt.Sprintf("Headline[%s](%d, %s, %v)", i.span.String(), i.headline.level, i.headline.body, i.headline.tags)
+	case commentItem:
+		return fmt.Sprintf("Comment[%s](%s)", i.span.String(), i.comment.body)
+	case keywordItem:
+		return fmt.Sprintf("Keyword[%s](%s: %s)", i.span.String(), i.keyword.keyword, i.keyword.value)
+	}
+	return "<invalid>"
+}
+
+type headline struct {
+	level int
+	body  string
+	tags  []string
+}
+
+type keyword struct {
+	keyword string
+	value   string
+}
+
+type comment struct {
+	body string
+}
+
+type text struct {
+	body string
 }
 
 type Scanner struct {
-	buffer  []byte
-	Pos     int
-	Line    int
-	LineLen int
-	LineBuf []byte
-	EOF     bool
+	input      string
+	pos        int
+	lineNumber int
+	lineLen    int
+	line       string
+	eof        bool
+	items      chan Item
 }
 
-func (s *Scanner) NextLine() bool {
-	if s.EOF {
-		s.LineBuf = nil
-		s.LineLen = 0
+func (s *Scanner) nextLine() bool {
+	if s.eof {
+		s.line = ""
+		s.lineLen = 0
 		return false
 	}
-	start := s.Pos
+	start := s.pos
 	p := start
-	for ; p < len(s.buffer) && s.buffer[p] != '\n'; p++ {
+	for ; p < len(s.input) && s.input[p] != '\n'; p++ {
 	}
-	s.LineBuf = s.buffer[start:p]
-	s.Line++
-	if p < len(s.buffer) {
-		s.Pos = p + 1
-		s.LineLen = p - start
+	s.line = s.input[start:p]
+	s.lineNumber++
+	if p < len(s.input) {
+		s.pos = p + 1
+		s.lineLen = p - start
 	} else {
-		s.Pos = p
-		s.LineLen = p - start
-		s.EOF = true
+		s.pos = p
+		s.lineLen = p - start
+		s.eof = true
 	}
 	return true
 }
 
-func (s *Scanner) Reset() {
-	s.buffer = s.buffer[:0]
-	s.Pos = 0
-	s.Line = 0
-	s.LineLen = 0
-	s.LineBuf = nil
-	s.EOF = false
+func (s *Scanner) reset() {
+	s.pos = 0
+	s.line = ""
+	s.lineNumber = 0
+	s.lineLen = 0
+	s.eof = false
 }
 
 func (s *Scanner) String() string {
-	return fmt.Sprintf("[%d:%d:%d]: %s\n", s.Line, s.Pos, s.LineLen, string(s.LineBuf))
+	return fmt.Sprintf("[%d:%d:%d]: %s\n", s.lineNumber, s.pos, s.lineLen, s.line)
 }
 
-func (s *Scanner) WithBytes(in []byte) {
-	s.Reset()
-	s.buffer = append(s.buffer, in...)
+func (s *Scanner) withString(in string) {
+	s.reset()
+	s.input = in
 }
 
-func NewScanner(in []byte) *Scanner {
-	r := &Scanner{}
-	r.WithBytes(in)
-	return r
-}
-
-type Point struct {
-	Line   int
-	Offset int
-}
-
-func (p Point) String() string {
-	return fmt.Sprintf("(%d:%d)", p.Line, p.Offset)
-}
-
-type Span struct {
-	Start Point
-	End   Point
-}
-
-func (s Span) String() string {
-	return fmt.Sprintf("%s - %s", s.Start, s.End)
-}
-
-type Node interface {
-	Span() Span
-	String() string
-}
-
-type State int
-
-const (
-	empty State = iota
-	keyword
-	text
-	headline
-	comment
-)
-
-type HeadlineNode struct {
-	span  Span
-	Level int
-	Body  string
-	Tags  []string
-}
-
-func (h *HeadlineNode) Span() Span {
-	return h.span
-}
-
-func (h *HeadlineNode) String() string {
-	return fmt.Sprintf("Headline[%d, '%s', [%s]; %s]", h.Level, h.Body, strings.Join(h.Tags, ", "), h.span)
-}
-
-type KeywordNode struct {
-	span    Span
-	Keyword string
-	Value   string
-}
-
-func (k *KeywordNode) String() string {
-	return fmt.Sprintf("Keyword[Key: %s, Value: %s; %s]", k.Keyword, k.Value, k.span)
-}
-
-func (k *KeywordNode) Span() Span {
-	return k.span
-}
-
-type CommentNode struct {
-	span Span
-	buf  *bytes.Buffer
-	Body string
-}
-
-func (c *CommentNode) String() string {
-	return fmt.Sprintf("Comment[%s; %s]", c.Body, c.span)
-}
-
-func (c *CommentNode) Span() Span {
-	return c.span
-}
-
-type TextNode struct {
-	span Span
-	buf  *bytes.Buffer
-	Body string
-}
-
-func (t *TextNode) Span() Span {
-	return t.span
-}
-
-func (t *TextNode) String() string {
-	return fmt.Sprintf("Text[%s; %s]", t.Body, t.span)
-}
-
-type Parser struct {
-	out     chan Node
-	scanner Scanner
-	state   State
-}
-
-func NewParser() (*Parser, <-chan Node) {
-	c := make(chan Node, 100)
-	return &Parser{
-		out: c,
-	}, c
-}
-
-// Reset resets the parser to the initial state
-func (p *Parser) Reset() {
-	p.scanner.Reset()
-	p.state = empty
+func NewScanner(in string) (*Scanner, chan Item) {
+	out := make(chan Item, 1000)
+	r := &Scanner{
+		input: in,
+		items: out,
+	}
+	r.withString(in)
+	return r, out
 }
 
 func isTagChar(r byte) bool {
 	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || r == '_' || r == '@'
 }
 
-func (p *Parser) currentPoint(offset int) Point {
-	return Point{
-		Line:   p.scanner.Line,
-		Offset: offset,
+func (s *Scanner) currentPoint(offset int) point {
+	return point{
+		line:   s.lineNumber,
+		offset: offset,
 	}
 }
 
-func (p *Parser) currentLine() int {
-	return p.scanner.Line
+func (s *Scanner) currentLine() int {
+	return s.lineNumber
 }
 
-func isHorizontalWhitespace(in byte) bool {
-	switch in {
-	case ' ', '\t':
-		return true
-	default:
-		return false
+func (s *Scanner) findWhitespaceDelimitedString(start, end int) string {
+	return strings.TrimSpace(s.line[start:end])
+}
+
+func (s *Scanner) currentWholeLine() span {
+	return span{
+		start: s.currentPoint(0),
+		end:   s.currentPoint(s.lineLen),
 	}
 }
 
-func (p *Parser) findWhitespaceDelimitedString(start, end int) string {
-	s := start
-	for ; isHorizontalWhitespace(p.scanner.LineBuf[s]); s++ {
+type scanFn func(scanner *Scanner) scanFn
+
+func tryComment(s *Scanner) scanFn {
+	m := commentRegexp.FindStringSubmatch(s.line)
+	if len(m) == 0 {
+		return nil
 	}
-	e := end
-	for ; isHorizontalWhitespace(p.scanner.LineBuf[e]); e-- {
+
+	body := bytes.Buffer{}
+	commentSpan := s.currentWholeLine()
+	body.WriteString(m[1])
+	var continueComment scanFn
+	continueComment = func(s *Scanner) scanFn {
+		if s.eof {
+			return nil
+		}
+
+		m := commentRegexp.FindStringSubmatch(s.line)
+		if len(m) == 0 {
+			commentSpan.end.line = s.lineNumber
+			commentSpan.end.offset = s.lineLen
+			i := Item{
+				itemType: commentItem,
+				span:     commentSpan,
+				comment: comment{
+					body: body.String(),
+				},
+			}
+			s.items <- i
+			return initialState
+		}
+
+		body.WriteByte('\n')
+		body.WriteString(m[1])
+		return continueComment
 	}
-	return string(p.scanner.LineBuf[s:e])
+	return continueComment
 }
 
-func (p *Parser) currentWholeLine() Span {
-	return Span{
-		Start: p.currentPoint(0),
-		End:   p.currentPoint(p.scanner.LineLen),
+func tryHeadline(s *Scanner) scanFn {
+	stars := 0
+	var pos int
+	var r rune
+	for pos, r = range s.line {
+		if r == '*' {
+			stars++
+			continue
+		}
+		if stars == 0 || r != ' ' {
+			return nil
+		}
+		break
 	}
-}
-
-type lexer struct {
-}
-
-type scanFn func(*lexer) scanFn
-
-func (p *Parser) Parse(file string, in []byte) error {
+	if stars == 0 {
+		return nil
+	}
+	headlineBody := ""
+	var headlineTags []string
+	in := s.findWhitespaceDelimitedString(pos, s.lineLen)
 	if len(in) == 0 {
 		return nil
 	}
-	p.scanner.WithBytes(in)
-
-	var node Node = nil
-	state := empty
-
-	closeMultiLineStructure := func() {}
-
-	var keywordKey string
-	var keywordValue string
-
-	isKeywordLine := func() bool {
-		m := keywordRegexp.FindSubmatch(p.scanner.LineBuf)
-		if len(m) > 0 {
-			keywordKey = string(m[1])
-			keywordValue = string(m[2])
-			return true
-		}
-		return false
-	}
-
-	var commentBody string
-
-	isCommentLine := func() bool {
-		m := commentRegexp.FindSubmatch(p.scanner.LineBuf)
-		if len(m) > 0 {
-			commentBody = string(m[1])
-			return true
-		}
-		return false
-	}
-
-	var headlineLevel int
-	var headlineBody string
-	var headlineTags []string
-
-	isHeadlineLine := func() bool {
-		stars := 0
-		var pos int
-		var r rune
-		for pos, r = range p.scanner.LineBuf {
-			if r == '*' {
-				stars++
+	if in[len(in)-1] == ':' {
+		lastColon := len(in) - 1
+		for p := lastColon - 1; p > 0; p-- {
+			c := in[p]
+			if isTagChar(c) {
 				continue
-			}
-			if stars == 0 || r != ' ' {
-				return false
-			}
-		}
-		headlineLevel = stars
-		headlineBody = ""
-		headlineTags = nil
-		in := p.findWhitespaceDelimitedString(pos, p.scanner.LineLen)
-		if len(in) == 0 {
-			return false
-		}
-		if in[len(in)-1] == ':' {
-			lastColon := len(in) - 1
-			for p := lastColon - 1; p > 0; p-- {
-				c := in[p]
-				if isTagChar(c) {
-					continue
-				} else if c == ':' {
-					if lastColon-p > 1 {
-						headlineTags = append(headlineTags, in[p+1:lastColon])
-					}
-					lastColon = p
-				} else {
-					break
+			} else if c == ':' {
+				if lastColon-p > 1 {
+					headlineTags = append(headlineTags, in[p+1:lastColon])
 				}
+				lastColon = p
+			} else {
+				break
 			}
-			headlineBody = strings.TrimSpace(in[:lastColon])
-		} else {
-			headlineBody = in
 		}
-		return true
+		headlineBody = strings.TrimSpace(in[:lastColon])
+	} else {
+		headlineBody = in
+	}
+	i := Item{
+		itemType: headlineItem,
+		span:     s.currentWholeLine(),
+		headline: headline{
+			level: stars,
+			body:  headlineBody,
+			tags:  headlineTags,
+		},
+	}
+	s.items <- i
+	return initialState
+}
+
+func tryKeyword(s *Scanner) scanFn {
+	m := keywordRegexp.FindStringSubmatch(s.line)
+	if len(m) == 0 {
+		return nil
+	}
+	keywordKey := m[1]
+	keywordValue := m[2]
+	i := Item{
+		itemType: keywordItem,
+		span:     s.currentWholeLine(),
+		keyword: keyword{
+			keyword: keywordKey,
+			value:   keywordValue,
+		},
+	}
+	s.items <- i
+	return initialState
+}
+
+func otherThanText(s *Scanner) scanFn {
+	var next scanFn
+	next = tryComment(s)
+	if next != nil {
+		return next
+	}
+	next = tryHeadline(s)
+	if next != nil {
+		return next
+	}
+	next = tryKeyword(s)
+	if next != nil {
+		return next
+	}
+	return nil
+}
+
+func tryText(s *Scanner) scanFn {
+	body := bytes.Buffer{}
+	textSpan := s.currentWholeLine()
+	body.WriteString(s.line)
+	var continueText scanFn
+	continueText = func(s *Scanner) scanFn {
+		emit := func() {
+			textSpan.end.line = s.currentLine()
+			textSpan.end.offset = s.lineLen
+			i := Item{
+				itemType: textItem,
+				span:     textSpan,
+				text: text{
+					body: body.String(),
+				},
+			}
+			s.items <- i
+		}
+
+		ott := otherThanText(s)
+		if ott != nil {
+			emit()
+			return ott
+		}
+		if s.eof {
+			emit()
+			return nil
+		}
+		body.WriteByte('\n')
+		body.WriteString(s.line)
+		return continueText
+	}
+	return continueText
+}
+
+func initialState(s *Scanner) scanFn {
+	if s.eof {
+		return nil
+	} else if s.pos >= len(s.input) {
+		s.eof = true
+		s.pos = len(s.input)
+		s.line = ""
+		return nil
 	}
 
-	for p.scanner.NextLine() {
-		switch {
-		case isKeywordLine():
-			closeMultiLineStructure()
-			state = empty
-			p.out <- &KeywordNode{
-				span:    p.currentWholeLine(),
-				Keyword: keywordKey,
-				Value:   strings.TrimSpace(keywordValue),
-			}
-		case isCommentLine():
-			var cn *CommentNode
-			if state == comment {
-				cn = node.(*CommentNode)
-				cn.buf.WriteByte('\n')
-				cn.span.End = p.currentPoint(0)
-			} else {
-				closeMultiLineStructure()
-				state = comment
-				cn = &CommentNode{
-					span: p.currentWholeLine(),
-					buf:  &bytes.Buffer{},
-				}
-				node = cn
-			}
-			cn.buf.WriteString(commentBody)
-		case isHeadlineLine():
-			closeMultiLineStructure()
-			state = empty
-			p.out <- &HeadlineNode{
-				span:  p.currentWholeLine(),
-				Level: headlineLevel,
-				Body:  headlineBody,
-				Tags:  headlineTags,
-			}
-		default:
-			var tn *TextNode
-			if state == text {
-				tn = node.(*TextNode)
-				tn.buf.WriteByte('\n')
-				tn.span.End = p.currentPoint(0)
-			} else {
-				closeMultiLineStructure()
-				state = text
-				tn = &TextNode{
-					span: p.currentWholeLine(),
-					buf:  &bytes.Buffer{},
-				}
-				node = tn
-			}
-			tn.buf.Write(p.scanner.LineBuf)
-		}
+	var next scanFn
+	next = tryComment(s)
+	if next != nil {
+		return next
 	}
+	next = tryHeadline(s)
+	if next != nil {
+		return next
+	}
+	next = tryKeyword(s)
+	if next != nil {
+		return next
+	}
+	return tryText(s)
+}
+
+func (s *Scanner) Scan() error {
+	s.nextLine()
+	for next := initialState(s); next != nil; next = next(s) {
+		s.nextLine()
+	}
+	close(s.items)
 	return nil
 }
